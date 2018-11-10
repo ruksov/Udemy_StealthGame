e415f89f573729567bccd797662b1eac924ee1f9
Yegor Krsochenko <e.ruksov@gmail.com>
2018-11-11 00:39:18 +0200

    Las lesson: 15
    Add logic to BlackHole class. Not work destoroying of actors
    when they overlapp inner BlackHole sphere component.

--- Source/FPSGame/Private/BlackHole.cpp
+++ Source/FPSGame/Private/BlackHole.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BlackHole.h"
-
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 ABlackHole::ABlackHole()
@@ -9,6 +10,22 @@ ABlackHole::ABlackHole()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+    MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+    MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    RootComponent = MeshComp;
+
+    InnerSphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("InnerSphereComp"));
+    InnerSphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    InnerSphereComp->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+    InnerSphereComp->SetCollisionResponseToChannel(ECollisionChannel::ECC_PhysicsBody, ECollisionResponse::ECR_Overlap);
+    InnerSphereComp->OnComponentBeginOverlap.AddDynamic(this, &ABlackHole::OnOverlapInnerSphere);
+    InnerSphereComp->SetupAttachment(MeshComp);
+
+    OuterSphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("OuterSphereComp"));
+    OuterSphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    OuterSphereComp->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+    OuterSphereComp->SetCollisionResponseToChannel(ECollisionChannel::ECC_PhysicsBody, ECollisionResponse::ECR_Overlap);
+    OuterSphereComp->SetupAttachment(MeshComp);
 }
 
 // Called when the game starts or when spawned
@@ -18,10 +35,35 @@ void ABlackHole::BeginPlay()
 	
 }
 
+void ABlackHole::OnOverlapInnerSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (OtherActor)
+    {
+        OtherActor->Destroy();
+    }
+}
+
 // Called every frame
 void ABlackHole::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+    /// Get overlapping components
+    TArray<UPrimitiveComponent*> OverlappingComponents;
+    OuterSphereComp->GetOverlappingComponents(OverlappingComponents);
+
+    /// Add radial force for each overlapping components
+    const float OuterSphereRadius = OuterSphereComp->GetScaledSphereRadius();
+    for (auto Comp : OverlappingComponents)
+    {
+        if (Comp && Comp->IsSimulatingPhysics())
+        {
+            Comp->AddRadialForce(GetActorLocation()
+                , OuterSphereRadius
+                , ForceStrength
+                , ERadialImpulseFalloff::RIF_Constant
+                , true);
+        }
+    }
 }
 
--- Source/FPSGame/Public/BlackHole.h
+++ Source/FPSGame/Public/BlackHole.h
@@ -15,14 +15,27 @@ public:
 	// Sets default values for this actor's properties
 	ABlackHole();
 
+    UPROPERTY(EditAnywhere, Category = "Config")
+    float ForceStrength = 0.f;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    class UStaticMeshComponent* MeshComp;
+
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    class USphereComponent* InnerSphereComp;
+
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    class USphereComponent* OuterSphereComp;
+
+    UFUNCTION()
+    void OnOverlapInnerSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
-	
 };
